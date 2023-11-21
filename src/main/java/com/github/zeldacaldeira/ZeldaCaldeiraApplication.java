bd90240487fa3e976a66ffaf17aa6fa0f2767eed
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-11-21 17:05:10 -0300

    first commit

---
+++ src/main/java/com/github/zeldacaldeira/ZeldaCaldeiraApplication.java
@@ -0,0 +1,13 @@
+package com.github.zeldacaldeira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeldaCaldeiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeldaCaldeiraApplication.class, args);
+	}
+
+}
