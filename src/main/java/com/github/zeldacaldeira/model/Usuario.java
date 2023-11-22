c7cc4578b3980c7d28eaaddf517a626bc781f061
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-11-22 19:00:39 -0300

    feat(Usuario): adiciona anotacao @Entity e @Table relacionando a tabela de testes(Usuarios)

--- src/main/java/com/github/zeldacaldeira/model/Usuario.java
+++ src/main/java/com/github/zeldacaldeira/model/Usuario.java
@@ -1,8 +1,6 @@
 package com.github.zeldacaldeira.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +10,8 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "Usuarios")
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
