cffe5813005816d7da18e1c274db9461ff3ba058
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-11-23 16:28:54 -0300

    style: renomea UserServiceApplication

--- user-service/src/main/java/com/github/zeldacaldeira/UserServiceApplication.java
+++ user-service/src/main/java/com/github/zeldacaldeira/UserServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZeldaCaldeiraApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZeldaCaldeiraApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
