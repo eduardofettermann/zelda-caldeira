d919e5760decc5f3bbc28cfbeda16da1233132fc
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-11-23 15:47:00 -0300

    chore: adiciona projeto para api externa

---
+++ zelda-service/src/main/java/com/github/zeldaservice/ZeldaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.zeldaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeldaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeldaServiceApplication.class, args);
+	}
+
+}
