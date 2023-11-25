700be745ac0e05a24e1959f33e4b58f80a701feb
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-11-25 11:19:46 -0300

    feat(user-service/model): adiciona classe para atualizar usuario UsuarioAtualizado

---
+++ user-service/src/main/java/com/github/zeldacaldeira/model/UsuarioAtualizado.java
@@ -0,0 +1,16 @@
+package com.github.zeldacaldeira.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioAtualizado {
+    private String nome;
+    private int idade;
+}
