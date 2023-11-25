5fee9089fb9a58a7eb694ee0c4286dfde187ea12
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-11-25 17:04:48 -0300

    refactor(user-service/service): compacta metodos e corrige UPDATE para ter resposta 404 ao tentar alterar Usuario nao existente

--- user-service/src/main/java/com/github/zeldacaldeira/service/UsuarioService.java
+++ user-service/src/main/java/com/github/zeldacaldeira/service/UsuarioService.java
@@ -1,11 +1,9 @@
 package com.github.zeldacaldeira.service;
 
 import com.github.zeldacaldeira.model.Usuario;
+import com.github.zeldacaldeira.model.UsuarioAtualizado;
 import com.github.zeldacaldeira.repository.UsuarioInterinoRepository;
-import org.aspectj.weaver.NewConstructorTypeMunger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,8 +14,11 @@ public class UsuarioService {
     @Autowired
     private UsuarioInterinoRepository usuarioInterinoRepository;
 
-    public Optional<Usuario> getUsuarioById(long id) {
-        return usuarioInterinoRepository.findById(id);
+    public Optional<Usuario> getUsuarioById(Long id) {
+        if (usuarioInterinoRepository.existsById(id)) {
+            return usuarioInterinoRepository.findById(id);
+        }
+        return Optional.empty();
     }
 
     public List<Usuario> getUsuarios() {
@@ -28,26 +29,16 @@ public class UsuarioService {
         return usuarioInterinoRepository.save(usuario);
     }
 
-    public ResponseEntity<Usuario> updateUsuario(long idUsuarioDesatualizado, Usuario usuarioAtualizado) {
-        ResponseEntity<Usuario> response;
-        if (usuarioInterinoRepository.existsById(idUsuarioDesatualizado)) {
-            usuarioAtualizado.setId(idUsuarioDesatualizado);
-            usuarioAtualizado = usuarioInterinoRepository.save(usuarioAtualizado);
-            response = ResponseEntity.ok(usuarioAtualizado);
-        } else {
-            response = ResponseEntity.notFound().build();
+    public Usuario updateUsuario(Long id, UsuarioAtualizado atualizacaoDoUsuario) {
+        if (usuarioInterinoRepository.existsById(id)) {
+            Usuario usuario = new Usuario(atualizacaoDoUsuario);
+            usuario.setId(id);
+            return usuarioInterinoRepository.save(usuario);
         }
-        return response;
+        return null;
     }
 
-    public ResponseEntity<?> deletarUsuario(long id) {
-        ResponseEntity<String> response;
-        if (usuarioInterinoRepository.existsById(id)) {
-            usuarioInterinoRepository.deleteById(id);
-            response = ResponseEntity.ok().build();
-        } else {
-            response = ResponseEntity.notFound().build();
-        }
-        return response;
+    public void deletarUsuario(long id) {
+        usuarioInterinoRepository.deleteById(id);
     }
 }
