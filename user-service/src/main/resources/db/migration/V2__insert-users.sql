dbd4ff71565148731008aff3c7d1249d9815fff8
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-11-26 20:26:08 -0300

    feat(user-service/migrations): adiciona V2 que insere os membros do grupo na tabelade Usuarios

---
+++ user-service/src/main/resources/db/migration/V2__insert-users.sql
@@ -0,0 +1,5 @@
+INSERT INTO Usuarios (nome, idade) VALUES ('Eduardo Bonatto', 60);
+INSERT INTO Usuarios (nome, idade) VALUES ('Eduardo Fettermann', 17);
+INSERT INTO Usuarios (nome, idade) VALUES ('Gaya', 60);
+INSERT INTO Usuarios (nome, idade) VALUES ('Giovane', 60);
+INSERT INTO Usuarios (nome, idade) VALUES ('Moises', 60);
