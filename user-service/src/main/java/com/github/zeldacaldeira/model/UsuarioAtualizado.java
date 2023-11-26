1236cce8d8b9e528412844b055f6d78e181549e4
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-11-26 19:17:43 -0300

    refactor(user-service/model): remove UsuarioAtualizado para UsuarioDTO

--- user-service/src/main/java/com/github/zeldacaldeira/model/UsuarioAtualizado.java
+++
@@ -1,4 +0,0 @@
-package com.github.zeldacaldeira.model;
-
-public record UsuarioAtualizado(String nome, int idade) {
-}
---
+++ user-service/src/main/java/com/github/zeldacaldeira/model/UsuarioDTO.java
@@ -0,0 +1,4 @@
+package com.github.zeldacaldeira.model;
+
+public record UsuarioDTO(String nome, int idade) {
+}
