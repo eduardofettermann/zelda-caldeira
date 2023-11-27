0a468b612a4b0181932df2eb89f5df2aa033d9e9
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2023-11-26 22:10:18 -0300

    refactor(user-service/config): incrementa método da classe SwaggerConfig

--- user-service/src/main/java/com/github/zeldacaldeira/config/SwaggerConfig.java
+++ user-service/src/main/java/com/github/zeldacaldeira/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.github.zeldacaldeira.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +13,13 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI custom () {
         return new OpenAPI().info(new Info().title("Zelda Caldeira").version("1.0.0")
-                .license(new License().name("System license").url("https://github.com/eduardofettermann/zelda-caldeira/tree/main"))
+                .description("\n" +
+                        "The objective of this project is to create an innovative platform that connects enthusiasts of the renowned Zelda franchise.")
+                .license(new License().name("Contact the developer").url("https://github.com/eduardofettermann/zelda-caldeira/tree/main"))
+                .contact(new Contact().name("Find out more about Swagger").url("https://swagger.io/"))
+
+
+
         );
     }
 }
