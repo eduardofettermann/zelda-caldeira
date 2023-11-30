c3191d39c5be1067c5a3e21b999e1ea6ca7cc493
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-11-30 14:38:31 -0300

    feat(zelda-service/service): adiciona repository como atributo e logica para verificar se ja existe o jogo no banco

--- zelda-service/src/main/java/com/github/zeldaservice/service/ZeldaService.java
+++ zelda-service/src/main/java/com/github/zeldaservice/service/ZeldaService.java
@@ -3,20 +3,37 @@ package com.github.zeldaservice.service;
 import com.github.zeldaservice.components.ZeldaWebClient;
 import com.github.zeldaservice.model.Jogo;
 import com.github.zeldaservice.model.TodosJogosResponse;
+import com.github.zeldaservice.repository.JogoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import java.util.Optional;
 
 @Service
 public class ZeldaService {
     @Autowired
     private ZeldaWebClient webClient;
+    @Autowired
+    private JogoRepository repository;
 
     public TodosJogosResponse getTodosJogosResponse() {
         return webClient.getTodosJogos();
     }
 
-    public Jogo getJogoById(String id) throws WebClientResponseException {
-        return webClient.getJogo(id).getJogoEncontrado();
+    public Optional<Jogo> getJogoById(String id) throws WebClientResponseException {
+        return verificaSeTemJogo(id) ? repository.findById(id) : Optional.empty();
+    }
+
+    public boolean verificaSeTemJogo(String id) {
+        if (repository.existsById(id)) {
+            return true;
+        }
+        try {
+            repository.save(webClient.getJogo(id).getJogoEncontrado());
+            return true;
+        } catch (WebClientResponseException e) {
+            return false;
+        }
     }
 }
