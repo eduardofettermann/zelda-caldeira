efda28db512609a2e053e6efc641e481aa5e6d4b
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-11-30 14:37:52 -0300

    feat(zelda-service/model): adiciona notacao de id no Jogo

--- zelda-service/src/main/java/com/github/zeldaservice/model/Jogo.java
+++ zelda-service/src/main/java/com/github/zeldaservice/model/Jogo.java
@@ -2,23 +2,32 @@ package com.github.zeldaservice.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.util.UUID;
+
+@Entity(name = "Jogo")
+@Table(name = "Jogos")
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Jogo {
 
     @JsonProperty(value = "name")
+    @Column(length = 1000)
     private String nome;
     @JsonProperty(value = "description")
+    @Column(length = 1000)
     private String descricao;
     @JsonProperty(value = "developer")
+    @Column(length = 1000)
     private String desenvolvedor;
     @JsonProperty(value = "publisher")
+    @Column(length = 1000)
     private String editor;
     @JsonProperty(value = "released_date")
     @Column(length = 1000)
     private String dataDeLancamento;
+    @Id
     private String id;
 }
