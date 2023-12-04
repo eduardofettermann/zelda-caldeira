c074f01445f1aa3a15898f13cc6e91ffec4bcc51
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-12-04 03:12:32 -0300

    feat(user-service/repository): adiciona metodo de busca de Usuario pelo login

--- user-service/src/main/java/com/github/zeldacaldeira/repository/UsuarioRepository.java
+++ user-service/src/main/java/com/github/zeldacaldeira/repository/UsuarioRepository.java
@@ -1,7 +1,9 @@
 package com.github.zeldacaldeira.repository;
 
-import com.github.zeldacaldeira.model.Usuario;
+import com.github.zeldacaldeira.model.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public  interface UsuarioRepository extends JpaRepository<Usuario,Long>{
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
 }
