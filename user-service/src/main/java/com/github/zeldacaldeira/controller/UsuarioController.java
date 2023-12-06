0361da002c978f95a4708eb1cca16731674b035d
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-12-06 20:20:28 -0300

    feat(user-service/controller): adiciona metodo favoritarJogo

--- user-service/src/main/java/com/github/zeldacaldeira/controller/UsuarioController.java
+++ user-service/src/main/java/com/github/zeldacaldeira/controller/UsuarioController.java
@@ -1,10 +1,10 @@
 package com.github.zeldacaldeira.controller;
 
 import com.github.zeldacaldeira.model.usuario.Usuario;
-import com.github.zeldacaldeira.model.usuario.AtualizacaoUsuario;
 import com.github.zeldacaldeira.service.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +16,16 @@ import java.util.Optional;
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
+
+    @PostMapping("/usuarios/{idDoUsuario}/favoritar/{idDoJogo}")
+    public ResponseEntity<Usuario> favoritarJogo(@PathVariable String idDoJogo, @PathVariable long idDoUsuario) throws DataIntegrityViolationException {
+        try {
+            Usuario usuario = usuarioService.favoritarJogo(idDoJogo, idDoUsuario);
+            return ResponseEntity.ok(usuario);
+        } catch (DataIntegrityViolationException exception) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 
     @Operation(summary = "Listar", description = "Método que retorna todos os usuários.", tags = "Usuário")
     @GetMapping("/usuarios")
