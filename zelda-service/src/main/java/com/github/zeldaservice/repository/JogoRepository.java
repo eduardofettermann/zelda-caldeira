0250b9558dac7b4206d2b30eea2be0a1cc448bb5
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-12-06 20:58:20 -0300

    refactor(zelda-caldeira): remove projeto zelda-service

--- zelda-service/src/main/java/com/github/zeldaservice/repository/JogoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.zeldaservice.repository;
-
-import com.github.zeldaservice.model.Jogo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface JogoRepository extends JpaRepository<Jogo, String> {
-}
