974b0725e343044cecd57db8df08f829d47b9ceb
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-12-05 23:36:59 -0300

    refactor(user-service/migrations): remove v2 do migrations pois nao fazia sentido

--- user-service/src/main/resources/db/migration/V2__insert-users.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Usuarios (login, senha, cargo, nome, idade) VALUES
-('Eduardo Bonatto@gmail.com', '$2a$10$mSzzn6pEM27p2.wL2yJZluxKJC3u4gsiC5CUOcSOJoh4/BfkJCyB', 'ADMINISTRADOR', 'Eduardo Bonatto', 60),
-('Eduardo Fettermann@gmail.com', '$2a$10$mSzzn6pEM27p2.wL2yJZluxKJC3u4gsiC5CUOcSOJoh4/BfkJCyB', 'ADMINISTRADOR', 'Eduardo Fettermann', 17),
-('Gaya@gmail.com', '$2a$10$mSzzn6pEM27p2.wL2yJZluxKJC3u4gsiC5CUOcSOJoh4/BfkJCyB', 'ADMINISTRADOR', 'Gaya', 60),
-('Giovane@gmail.com', '$2a$10$mSzzn6pEM27p2.wL2yJZluxKJC3u4gsiC5CUOcSOJoh4/BfkJCyB', 'ADMINISTRADOR', 'Giovane', 60),
-('Moises@gmail.com', '$2a$10$mSzzn6pEM27p2.wL2yJZluxKJC3u4gsiC5CUOcSOJoh4/BfkJCyB', 'ADMINISTRADOR', 'Moises', 60);
