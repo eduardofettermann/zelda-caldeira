fec46d5a910ea2d4cecb5969a153a13343c02827
ebonatto <ebonatto.dev@gmail.com>
2023-12-06 00:14:14 -0300

    feat: cors config

--- zelda-service/src/main/java/com/github/zeldaservice/ZeldaServiceApplication.java
+++ zelda-service/src/main/java/com/github/zeldaservice/ZeldaServiceApplication.java
@@ -2,6 +2,10 @@ package com.github.zeldaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class ZeldaServiceApplication {
@@ -10,4 +14,17 @@ public class ZeldaServiceApplication {
 		SpringApplication.run(ZeldaServiceApplication.class, args);
 	}
 
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*") // Permitir solicitações de qualquer origem
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("Content-Type");
+			}
+		};
+	}
 }
