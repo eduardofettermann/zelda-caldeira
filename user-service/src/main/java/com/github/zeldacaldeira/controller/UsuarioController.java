a4e5017c4eb3c06b302a1a8e943ddd094fcb9d0b
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-02 20:21:41 -0300

    feat(user-service): atualiza controller

--- user-service/src/main/java/com/github/zeldacaldeira/controller/UsuarioController.java
+++ user-service/src/main/java/com/github/zeldacaldeira/controller/UsuarioController.java
@@ -1,9 +1,8 @@
 package com.github.zeldacaldeira.controller;
 
+import com.github.zeldacaldeira.model.usuario.CadastroDTO;
 import com.github.zeldacaldeira.model.usuario.Usuario;
-import com.github.zeldacaldeira.model.usuario.AtualizacaoUsuario;
 import com.github.zeldacaldeira.service.UsuarioService;
-import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,39 +16,28 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    @Operation(summary = "Listar", description = "Método que retorna todos os usuários.", tags = "Usuário")
     @GetMapping("/usuarios")
-    public List<Usuario> retornarUsuarios() {
-        return usuarioService.getUsuarios();
+    public List<Usuario> listaUsuarios() {
+        return usuarioService.listaUsuarios();
     }
 
-    @Operation(summary = "Busca por id", description = "Método que retorna um usuário.", tags = "Usuário")
     @GetMapping("/usuarios/{id}")
-    public ResponseEntity<Optional<Usuario>> retornarUsuarioPorId(@PathVariable Long id) {
-        return usuarioService.getUsuarioById(id).isPresent() ?
-                ResponseEntity.ok(usuarioService.getUsuarioById(id)) :
+    public ResponseEntity<Optional<Usuario>> listaUsuarioPorId(@PathVariable Long id) {
+        return usuarioService.listaUsuarioById(id).isPresent() ?
+                ResponseEntity.ok(usuarioService.listaUsuarioById(id)) :
                 ResponseEntity.notFound().build();
     }
 
-//    @Operation(summary = "Cadastrar", description = "Método que cadastra um usuário.", tags = "Usuário")
-//    @PostMapping("/usuarios/cadastrar")
-//    public Usuario salvarUsuario(@RequestBody AtualizacaoUsuario atualizacaoUsuario) {
-//        return usuarioService.saveUsuario(atualizacaoUsuario);
-//    }
+    @PostMapping("/usuarios/cadastra")
+    public ResponseEntity<Usuario> salvaUsuario(@RequestBody CadastroDTO cadastroDTO) {
+        Usuario usuario = usuarioService.salvaUsuario(cadastroDTO);
+        return ResponseEntity.ok(usuario);
+    }
 
-//    @Operation(summary = "Atualizar", description = "Método que atualiza um usuário.", tags = "Usuário")
-//    @PutMapping("/usuarios/atualizar/{id}")
-//    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody AtualizacaoUsuario atualizacaoDoUsuario) {
-//        return usuarioService.updateUsuario(id, atualizacaoDoUsuario) != null ?
-//                ResponseEntity.ok(usuarioService.updateUsuario(id, atualizacaoDoUsuario)) :
-//                ResponseEntity.notFound().build();
-//    }
-
-    @Operation(summary = "Deletar", description = "Método que deleta um usuário.", tags = "Usuário")
-    @DeleteMapping("/usuarios/deletar/{id}")
+    @DeleteMapping("/usuarios/deleta/{id}")
     public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
-        if (usuarioService.deletarUsuario(id)) {
-            usuarioService.deletarUsuario(id);
+        if (usuarioService.deletaUsuario(id)) {
+            usuarioService.deletaUsuario(id);
             return ResponseEntity.ok().build();
         }
         return ResponseEntity.notFound().build();
