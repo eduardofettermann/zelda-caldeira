988cd5d614ec8ebce4852c57690fbcd27b7d6598
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-02 20:36:09 -0300

    refactor(user-service): remove imports nao utilizados

--- user-service/src/main/java/com/github/zeldacaldeira/repository/UsuarioRepository.java
+++ user-service/src/main/java/com/github/zeldacaldeira/repository/UsuarioRepository.java
@@ -2,7 +2,6 @@ package com.github.zeldacaldeira.repository;
 
 import com.github.zeldacaldeira.model.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Usuario findByEmail(String email);
