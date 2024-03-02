8fa88c9f3bb0c3b3952a3ea048f3139074262409
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-02 20:17:03 -0300

    refactor(user-service): remove DTOs depreciadas

--- user-service/src/main/java/com/github/zeldacaldeira/model/usuario/LoginDTO.java
+++
@@ -1,4 +0,0 @@
-package com.github.zeldacaldeira.model.usuario;
-
-public record LoginDTO(String login, String senha) {
-}
--- user-service/src/main/java/com/github/zeldacaldeira/model/usuario/LoginResponseDTO.java
+++
@@ -1,4 +0,0 @@
-package com.github.zeldacaldeira.model.usuario;
-
-public record LoginResponseDTO(String token) {
-}
