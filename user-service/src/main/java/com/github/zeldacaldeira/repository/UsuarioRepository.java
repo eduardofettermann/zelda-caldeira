9280ac9c9fcb5ff602d127fc9b074f128484d303
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-02 20:21:27 -0300

    feat(user-service): troca nome do metodo de busca por email

--- user-service/src/main/java/com/github/zeldacaldeira/repository/UsuarioRepository.java
+++ user-service/src/main/java/com/github/zeldacaldeira/repository/UsuarioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    UserDetails findByLogin(String login);
+    Usuario findByEmail(String email);
 }
