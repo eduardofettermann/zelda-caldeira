167155a2956b73b0155d0e0a7537e0415588cfff
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-02 21:42:14 -0300

    refactor(user-service): atualiza CadastroDTO

--- user-service/src/main/java/com/github/zeldacaldeira/model/CadastroDTO.java
+++ user-service/src/main/java/com/github/zeldacaldeira/model/CadastroDTO.java
@@ -5,7 +5,6 @@ import jakarta.annotation.Nullable;
 public record CadastroDTO(
         String email,
         String senha,
-        @Nullable CargoEnum cargoEnum,
         String nome,
         int idade) {
 }
