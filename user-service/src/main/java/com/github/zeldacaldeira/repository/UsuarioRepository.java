935e2571a857e46edf1c5c814c2ed5326b20e4db
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-02 21:40:20 -0300

    refactor(user-service): remove package usuario

--- user-service/src/main/java/com/github/zeldacaldeira/repository/UsuarioRepository.java
+++ user-service/src/main/java/com/github/zeldacaldeira/repository/UsuarioRepository.java
@@ -1,6 +1,6 @@
 package com.github.zeldacaldeira.repository;
 
-import com.github.zeldacaldeira.model.usuario.Usuario;
+import com.github.zeldacaldeira.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
--- user-service/src/main/java/com/github/zeldacaldeira/service/UsuarioService.java
+++ user-service/src/main/java/com/github/zeldacaldeira/service/UsuarioService.java
@@ -1,8 +1,8 @@
 package com.github.zeldacaldeira.service;
 
-import com.github.zeldacaldeira.model.usuario.AtualizacaoUsuarioDTO;
-import com.github.zeldacaldeira.model.usuario.CadastroDTO;
-import com.github.zeldacaldeira.model.usuario.Usuario;
+import com.github.zeldacaldeira.model.AtualizacaoUsuarioDTO;
+import com.github.zeldacaldeira.model.CadastroDTO;
+import com.github.zeldacaldeira.model.Usuario;
 import com.github.zeldacaldeira.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,7 +30,7 @@ public class UsuarioService {
         return usuarioRepository.save(new Usuario(cadastroDTO));
     }
 
-    public Usuario atualizaUsuario(Long id, AtualizacaoUsuarioDTO atualizacaoUsuarioDTO) {
+    public Usuario atualizaUsuario(Long id, AtualizacaoUsuarioDTO atualizacaoUsuarioDTO){
         if (usuarioRepository.existsById(id)) {
             Usuario usuario = usuarioRepository.findById(id).get();
             usuario.atualizarUsuario(atualizacaoUsuarioDTO);
