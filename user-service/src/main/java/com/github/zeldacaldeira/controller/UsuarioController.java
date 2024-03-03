c5ed47a2eb273117b4686d8948bae5f1fd7be251
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-02 21:40:37 -0300

    feat(user-service): adiciona metodo update

--- user-service/src/main/java/com/github/zeldacaldeira/controller/UsuarioController.java
+++ user-service/src/main/java/com/github/zeldacaldeira/controller/UsuarioController.java
@@ -1,7 +1,8 @@
 package com.github.zeldacaldeira.controller;
 
-import com.github.zeldacaldeira.model.usuario.CadastroDTO;
-import com.github.zeldacaldeira.model.usuario.Usuario;
+import com.github.zeldacaldeira.model.AtualizacaoUsuarioDTO;
+import com.github.zeldacaldeira.model.CadastroDTO;
+import com.github.zeldacaldeira.model.Usuario;
 import com.github.zeldacaldeira.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -32,6 +33,12 @@ public class UsuarioController {
     public ResponseEntity<Usuario> salvaUsuario(@RequestBody CadastroDTO cadastroDTO) {
         Usuario usuario = usuarioService.salvaUsuario(cadastroDTO);
         return ResponseEntity.ok(usuario);
+    }
+
+    @PutMapping("/usuarios/atualiza/{id}")
+    public ResponseEntity<Usuario> atualizaUsuario(@RequestBody AtualizacaoUsuarioDTO atualizacaoUsuarioDTO, @PathVariable long id) {
+        var retornoDaAtualizacao = usuarioService.atualizaUsuario(id, atualizacaoUsuarioDTO);
+        return retornoDaAtualizacao != null ? ResponseEntity.ok(retornoDaAtualizacao) : ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/usuarios/deleta/{id}")
